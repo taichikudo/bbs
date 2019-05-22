0c5e4aec0a7b0b6a0f74836e24eda119e43d0da9
taichi kudo <php.kudo@gmail.com>
2019-05-22 14:24:35 +0900

    5

--- src/Model/Entity/Rental.php
+++ src/Model/Entity/Rental.php
@@ -3,19 +3,6 @@ namespace App\Model\Entity;
 
 use Cake\ORM\Entity;
 
-/**
- * Rental Entity
- *
- * @property int $id
- * @property int $rental_user_id
- * @property int $rental_book_id
- * @property \Cake\I18n\FrozenDate $rental_date
- * @property \Cake\I18n\FrozenDate $rental_return
- * @property string $rental_etc
- *
- * @property \App\Model\Entity\RentalUser $rental_user
- * @property \App\Model\Entity\RentalBook $rental_book
- */
 class Rental extends Entity
 {
     /**
--- src/Model/Table/RentalTable.php
+++ src/Model/Table/RentalTable.php
@@ -6,29 +6,8 @@ use Cake\ORM\RulesChecker;
 use Cake\ORM\Table;
 use Cake\Validation\Validator;
 
-/**
- * Rental Model
- *
- * @property \App\Model\Table\RentalUsersTable|\Cake\ORM\Association\BelongsTo $RentalUsers
- * @property \App\Model\Table\RentalBooksTable|\Cake\ORM\Association\BelongsTo $RentalBooks
- *
- * @method \App\Model\Entity\Rental get($primaryKey, $options = [])
- * @method \App\Model\Entity\Rental newEntity($data = null, array $options = [])
- * @method \App\Model\Entity\Rental[] newEntities(array $data, array $options = [])
- * @method \App\Model\Entity\Rental|bool save(\Cake\Datasource\EntityInterface $entity, $options = [])
- * @method \App\Model\Entity\Rental saveOrFail(\Cake\Datasource\EntityInterface $entity, $options = [])
- * @method \App\Model\Entity\Rental patchEntity(\Cake\Datasource\EntityInterface $entity, array $data, array $options = [])
- * @method \App\Model\Entity\Rental[] patchEntities($entities, array $data, array $options = [])
- * @method \App\Model\Entity\Rental findOrCreate($search, callable $callback = null, $options = [])
- */
 class RentalTable extends Table
 {
-    /**
-     * Initialize method
-     *
-     * @param array $config The configuration for the Table.
-     * @return void
-     */
     public function initialize(array $config)
     {
         parent::initialize($config);
@@ -37,22 +16,9 @@ class RentalTable extends Table
         $this->setDisplayField('id');
         $this->setPrimaryKey('id');
 
-        $this->belongsTo('RentalUsers', [
-            'foreignKey' => 'rental_user_id',
-            'joinType' => 'INNER'
-        ]);
-        $this->belongsTo('RentalBooks', [
-            'foreignKey' => 'rental_book_id',
-            'joinType' => 'INNER'
-        ]);
+
     }
 
-    /**
-     * Default validation rules.
-     *
-     * @param \Cake\Validation\Validator $validator Validator instance.
-     * @return \Cake\Validation\Validator
-     */
     public function validationDefault(Validator $validator)
     {
         $validator
@@ -78,18 +44,4 @@ class RentalTable extends Table
         return $validator;
     }
 
-    /**
-     * Returns a rules checker object that will be used for validating
-     * application integrity.
-     *
-     * @param \Cake\ORM\RulesChecker $rules The rules object to be modified.
-     * @return \Cake\ORM\RulesChecker
-     */
-    public function buildRules(RulesChecker $rules)
-    {
-        $rules->add($rules->existsIn(['rental_user_id'], 'RentalUsers'));
-        $rules->add($rules->existsIn(['rental_book_id'], 'RentalBooks'));
-
-        return $rules;
-    }
-}
+  }
